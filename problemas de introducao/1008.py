ab159a90f2a03defd3245f9e55003a0007f3d36f
WendellVG <122946577+WendellVG@users.noreply.github.com>
2023-03-19 21:21:34 -0300

    .

---
+++ problemas de introducao/1008.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+c=float(input())
+
+
+print(f'NUMBER = {a}')
+print(f'SALARY = U$ {b*c:.2f}')
---
+++ problemas de introducao/1009.py
@@ -0,0 +1,5 @@
+a=input()
+b=float(input())
+c=float(input())
+cms=c*0.15
+print(f'TOTAL = R$ {cms+b:.2f}')
