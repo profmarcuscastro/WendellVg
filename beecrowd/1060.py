8b6f54fe42e4dfb9bd2d947c6767a2fb74b71e13
WendellVG <122946577+WendellVG@users.noreply.github.com>
2023-04-09 20:51:03 -0300

    .

---
+++ beecrowd/1060.py
@@ -0,0 +1,7 @@
+pst = 0
+for i in range(6):
+    x = float(input())
+
+    if x>0:
+        pst=pst+1
+print(f'{pst} valores positivos')
---
+++ beecrowd/1065.py
@@ -0,0 +1,7 @@
+pst = 0
+for i in range(5):
+    x = float(input())
+
+    if x%2==0:
+        pst=pst+1
+print(f'{pst} valores positivos')
